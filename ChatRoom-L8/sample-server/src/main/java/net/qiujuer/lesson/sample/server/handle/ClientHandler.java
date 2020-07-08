2a6cede320e6f76f81924e16719748dbafe532aa
belief.cn <673727003@qq.com>
2020-07-08 22:05:37 +0800

    数据传输稳定性优化

---
+++ ChatRoom-L8/sample-server/src/main/java/net/qiujuer/lesson/sample/server/handle/ClientHandler.java
@@ -0,0 +1,52 @@
+package net.qiujuer.lesson.sample.server.handle;
+
+
+import net.qiujuer.library.clink.core.Connector;
+import net.qiujuer.library.clink.utils.CloseUtils;
+
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+
+public class ClientHandler extends Connector {
+    private final ClientHandlerCallback clientHandlerCallback;
+    private final String clientInfo;
+
+    public ClientHandler(SocketChannel socketChannel, ClientHandlerCallback clientHandlerCallback) throws IOException {
+        this.clientHandlerCallback = clientHandlerCallback;
+        this.clientInfo = socketChannel.getRemoteAddress().toString();
+
+        System.out.println("新客户端连接：" + clientInfo);
+
+        setup(socketChannel);
+    }
+
+    public void exit() {
+        CloseUtils.close(this);
+        System.out.println("客户端已退出：" + clientInfo);
+    }
+
+    @Override
+    public void onChannelClosed(SocketChannel channel) {
+        super.onChannelClosed(channel);
+        exitBySelf();
+    }
+
+    private void exitBySelf() {
+        exit();
+        clientHandlerCallback.onSelfClosed(this);
+    }
+
+    @Override
+    protected void onReceiveNewMessage(String str) {
+        super.onReceiveNewMessage(str);
+        clientHandlerCallback.onNewMessageArrived(this, str);
+    }
+
+    public interface ClientHandlerCallback {
+        // 自身关闭通知
+        void onSelfClosed(ClientHandler handler);
+
+        // 收到消息时通知
+        void onNewMessageArrived(ClientHandler handler, String msg);
+    }
+}
